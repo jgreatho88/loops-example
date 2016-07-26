df3b4bf6b08f711c138df4eac9c0b12968763c3d
John Greathouse <johngreathouse@johns-imac.local>
2016-07-26 13:48:55 -0500

    all loops accounted for

--- loops-example/ViewController.swift
+++ loops-example/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLbl: UILabel!
+    
+    var phrases = ["Booting from floppy...\n", "Reading from disk...\n", "Updating Registry...\n", "................\n", ".............\n", "Welcome, John. \n It is nice to see you again"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainLbl.text = ""
+        
+//        for x in 0 ..< phrases.count {
+//            var txt = mainLbl.text!
+//            txt += phrases[x]
+//            mainLbl.text = txt }
+    
+    
+//    var x = 0
+//        
+//    repeat {
+//                var txt = mainLbl.text!
+//                txt += phrases[x]
+//                mainLbl.text = txt
+//                x += 1
+//            } while x < phrases.count
+        
+        for phrase in phrases {
+            
+            var txt = mainLbl.text!
+            txt += phrase
+            mainLbl.text = txt
+        
+        }
+        
+        
     }
 
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
